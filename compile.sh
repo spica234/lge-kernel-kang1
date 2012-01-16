7075879b4fe7594bcd1a877a36ea30b09bf9d517
vadonka <vadonka@vadonka.com>
2012-01-16 14:30:16 +0100

    Update compiler script

--- compile.sh
+++ compile.sh
@@ -1,16 +1,48 @@
 #!/bin/sh
-# ANYKERNEL compiler script by vadonka v1.0.2
-# Date: 2012.01.04
+# ANYKERNEL compiler script by vadonka v1.0.5
+# Date: 2012.01.16
 #
 # You need to define this below:
 ######################################################
 # KERNEL home directory
-export krnlhome=/home/android/android/lge-kernel-kang
+export krnlhome=`pwd`
 # Compiled files home directory
 export comphome=/home/android/android/compiled
 # CM7 original lge kernel boot.img location
 export cm7bootimg=/home/android/android/cm7orig_kernel
 ######################################################
+
+CBOOST=`grep -c "^CONFIG_NVRM_CPU1_CORE_BOOST" $krnlhome/.config`
+AVPOC=`grep -c "^CONFIG_MAX_AVP_OC_FREQ_BOOST" $krnlhome/.config`
+GPUOC=`grep -c "^CONFIG_MAX_3D_OC_FREQ_BOOST" $krnlhome/.config`
+RAMHACK=`grep "^CONFIG_RAMHACK" $krnlhome/.config | awk 'BEGIN { FS = "=" } ; { print $2}'`
+CVER=`grep "^CONFIG_LOCALVERSION" $krnlhome/.config`
+
+if [ "$CBOOST" == "1" -a "$AVPOC" == "1" -a "$GPUOC" == "1" ]; then
+	NVER=`echo 'CONFIG_LOCALVERSION="-LGEK-AGOCCB-'$(($RAMHACK))'M"'`
+	sed -i "s/$CVER/$NVER/g" $krnlhome/.config
+elif [ "$CBOOST" == "1" -a "$AVPOC" == "1" -a "$GPUOC" == "0" ]; then
+	NVER=`echo 'CONFIG_LOCALVERSION="-LGEK-AOCCB-'$(($RAMHACK))'M"'`
+	sed -i "s/$CVER/$NVER/g" $krnlhome/.config
+elif [ "$CBOOST" == "1" -a "$AVPOC" == "0" -a "$GPUOC" == "1" ]; then
+	NVER=`echo 'CONFIG_LOCALVERSION="-LGEK-GOCCB-'$(($RAMHACK))'M"'`
+	sed -i "s/$CVER/$NVER/g" $krnlhome/.config
+elif [ "$CBOOST" == "1" -a "$AVPOC" == "0" -a "$GPUOC" == "0" ]; then
+	NVER=`echo 'CONFIG_LOCALVERSION="-LGEK-CB-'$(($RAMHACK))'M"'`
+	sed -i "s/$CVER/$NVER/g" $krnlhome/.config
+elif [ "$CBOOST" == "0" -a "$AVPOC" == "1" -a "$GPUOC" == "1" ]; then
+	NVER=`echo 'CONFIG_LOCALVERSION="-LGEK-AGOC-'$(($RAMHACK))'M"'`
+	sed -i "s/$CVER/$NVER/g" $krnlhome/.config
+elif [ "$CBOOST" == "0" -a "$AVPOC" == "1" -a "$GPUOC" == "0" ]; then
+	NVER=`echo 'CONFIG_LOCALVERSION="-LGEK-AOC-'$(($RAMHACK))'M"'`
+	sed -i "s/$CVER/$NVER/g" $krnlhome/.config
+elif [ "$CBOOST" == "0" -a "$AVPOC" == "0" -a "$GPUOC" == "1" ]; then
+	NVER=`echo 'CONFIG_LOCALVERSION="-LGEK-GOC-'$(($RAMHACK))'M"'`
+	sed -i "s/$CVER/$NVER/g" $krnlhome/.config
+else
+	NVER=`echo 'CONFIG_LOCALVERSION="-LGEK-PwrS-'$(($RAMHACK))'M"'`
+	sed -i "s/$CVER/$NVER/g" $krnlhome/.config
+fi
 
 export CCOMPILER=arm-linux-gnueabi-
 export USE_CCACHE=1
@@ -18,10 +50,11 @@ make clean
 make ARCH=arm CROSS_COMPILE=$CCOMPILER clean
 make ARCH=arm CROSS_COMPILE=$CCOMPILER
 
+if [ -e $krnlhome/arch/arm/boot/zImage ]; then
 mem=383
 nvmem1=128
 nvmem2=384
-RAMHACK=`grep "^CONFIG_RAMHACK" $krnlhome/.config | awk 'BEGIN { FS = "=" } ; { print $2}'`
+
 let mem=$mem+$RAMHACK
 let nvmem1=$nvmem1-$RAMHACK
 let nvmem2=$nvmem2+$RAMHACK
@@ -45,3 +78,4 @@ usbcore.old_scheme_first=1 tegraboot=sdmmc tegrapart=recovery:35e00:2800:800,lin
 mbr:400:200:800,system:600:2bc00:800,cache:2c200:8000:800,misc:34200:400:800,\
 userdata:38700:c0000:800 androidboot.hardware=p990"
 abootimg -i $COMPDIR/boot.img > $COMPDIR/boot.img.info
+fi
