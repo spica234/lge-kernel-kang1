1fe380443e2de04b3eaa344bc88b4ff20ac2c521
vadonka <vadonka@vadonka.com>
2012-01-17 09:13:51 +0100

    update scripts

---
+++ build-all.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+./initialize.sh vadonka_pwrs_defconfig
+./compile.sh 32
--- initialize.sh
+++ initialize.sh
@@ -3,8 +3,18 @@
 export CCOMPILER=arm-linux-gnueabi-
 export USE_CCACHE=1
 
+if [ -e .config ]; then
+	rm -f .config
+fi
+
+if [ -z $1 ]; then
+	export DEFCONFIG="vadonka_defconfig"
+else
+	export DEFCONFIG="$1"
+fi
+
 make clean
 make CROSS_COMPILE=$CCOMPILER clean
 make ARCH=arm CROSS_COMPILE=$CCOMPILER clean
 make ARCH=arm CROSS_COMPILE=$CCOMPILER mrproper
-make ARCH=arm CROSS_COMPILE=$CCOMPILER vadonka_defconfig
+make ARCH=arm CROSS_COMPILE=$CCOMPILER $DEFCONFIG
