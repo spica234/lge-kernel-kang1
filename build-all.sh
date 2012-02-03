7b2a6f26a012b9751ef8601c9f4d3118196fab35
vadonka <vadonka@vadonka.com>
2012-02-03 16:57:08 +0100

    Change builder script

--- build-all.sh
+++ build-all.sh
@@ -4,20 +4,14 @@
 ./initialize.sh
 ./compile.sh 0
 #32m shared
-./initialize.sh
 ./compile.sh 32 shared
 #32m
-./initialize.sh
 ./compile.sh 32
 #48m
-./initialize.sh
 ./compile.sh 48
 #64m
-./initialize.sh
 ./compile.sh 64
 #80m
-./initialize.sh
 ./compile.sh 80
 #96m
-./initialize.sh
 ./compile.sh 96
