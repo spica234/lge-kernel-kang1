49ecf3205f3e935c61637d0f3cf9c0be683de762
vadonka <vadonka@vadonka.com>
2012-02-08 08:11:27 +0100

    Lowering sdram freq

--- arch/arm/mach-tegra/nvrm/core/common/nvrm_clocks_limits.c
+++ arch/arm/mach-tegra/nvrm/core/common/nvrm_clocks_limits.c
@@ -290,9 +290,9 @@ NvRmPrivClockLimitsInit(NvRmDeviceHandle hRmDevice)
     }
 
         s_ClockRangeLimits[2].MaxKHz = 240000;
-        s_ClockRangeLimits[7].MaxKHz = 325000;
+        s_ClockRangeLimits[7].MaxKHz = 300000;
         s_ClockRangeLimits[8].MaxKHz = 345000;
-        s_ClockRangeLimits[10].MaxKHz = 325000;
+        s_ClockRangeLimits[10].MaxKHz = 300000;
 
     // Fill in CPU scaling data if SoC has dedicated CPU rail, and CPU clock
     // characterization data is separated from other modules on common core rail
